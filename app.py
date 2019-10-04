7e7b6fa013aa14888863b37b82eb31ed5664a31c
dafuqkrjiohhy <51810363+dafuqkrjiohhy@users.noreply.github.com>
2019-10-04 13:36:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def callback():
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
     if (usertext == 'สวัสดี') :
-       sendText(user,'เออจ้า')
+        sendText(user,'เออจ้า')
     elif (usertext =='ปมนยองเป็นอะไรกัน') :
-       sendText(user,'แฟนกันดูออก')
+        sendText(user,'แฟนกันดูออก')
     else :
-       sendText(user,'ไม่รับจ้า')
+        sendText(user,'ไม่รับจ้า')
 
     return '',200
 
